bd3edf7b524c52513a7139fbfc8a845151d23c97
asdawsd@cwewc.cew <asdawsd@cwewc.cew>
2024-12-21 13:08:35 +0700

    fix setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = {}
 with open("version.py") as version_file:
     exec(version_file.read(), version)
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("readme.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
